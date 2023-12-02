f280f22da058b8ff9dcace31214d5b778ec7452a
Francinei Silva <francineisilva7@gmail.com>
2023-12-02 20:21:14 -0300

    Variáveis

---
+++ ex002/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Validando</title>
+</head>
+<body>
+    <h1>Exemplos no PHP</h1>
+    <!-- As variáveis em PHP usam o $ na frente do nome -->
+    <?php 
+        
+        $nome = "Francinei";
+        $sobrenome = "Silva";
+        echo "Muito prazer, $nome $sobrenome";
+    
+    ?>
+</body>
+</html>
