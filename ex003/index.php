64853c09ecd7cfac7c4d178a88b145cd64fcaf6c
Francinei Silva <francineisilva7@gmail.com>
2023-12-03 15:19:21 -0300

    Coersão / Tipo primitivo

---
+++ ex003/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tipos Primitivos</title>
+</head>
+<body>
+    <h1>Testes de tipos primitivos</h1>
+    <?php 
+        // //0x = hexadecimal | 0b = binário | 0 = octal
+        // $num = 300;
+        // echo "O valor da variável é $num";
+
+        // $v = 95.4;
+        // var_dump($v);
+
+        $num = (float) "950"; // coersão - forçar a ser um tipo específico
+        // echo "O valor é $num";
+        var_dump($num);
+
+    ?>
+</body>
+</html>
